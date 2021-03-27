8858455b3e22b2606d19103c24d6db6d03054a6f
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2021-03-27 13:52:59 +0200

    Bug fixing on recipe filters

--- app/src/main/java/com/stathis/foodie/ui/dashboard/filters/FiltersFragment.kt
+++ app/src/main/java/com/stathis/foodie/ui/dashboard/filters/FiltersFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.foodie.R
 import com.stathis.foodie.abstraction.AbstractFragment
@@ -32,12 +33,24 @@ class FiltersFragment : AbstractFragment(R.layout.fragment_filters) {
         diet_autocomplete.setAdapter(dietTypeAdapter)
 
         find_recipes_floating_btn.setOnClickListener {
-            startActivity(Intent(requireContext(), FilterResultsActivity::class.java).also{
-                it.putExtra("KCAL_MIN_VALUE",calories_slider.values[0].toDouble())
-                it.putExtra("KCAL_MAX_VALUE",calories_slider.values[1].toDouble())
-                it.putExtra("MEAL_TYPE",meal_type_autocomplete.text.toString())
-                it.putExtra("DIET_TYPE",diet_autocomplete.text.toString())
-            })
+
+            when (viewModel.checkIfValid(
+                calories_slider.values[0].toString(),
+                calories_slider.values[1].toString(),
+                meal_type_autocomplete.text.toString(),
+                diet_autocomplete.text.toString()
+            )) {
+                true -> {
+                    startActivity(Intent(requireContext(), FilterResultsActivity::class.java).also {
+                        it.putExtra("KCAL_MIN_VALUE", calories_slider.values[0].toDouble())
+                        it.putExtra("KCAL_MAX_VALUE", calories_slider.values[1].toDouble())
+                        it.putExtra("MEAL_TYPE", meal_type_autocomplete.text.toString())
+                        it.putExtra("DIET_TYPE", diet_autocomplete.text.toString())
+                    })
+                }
+                false -> Toast.makeText(context, resources.getText(R.string.toast_fill_all_fields), Toast.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 
--- app/src/main/java/com/stathis/foodie/ui/dashboard/filters/FiltersViewModel.kt
+++ app/src/main/java/com/stathis/foodie/ui/dashboard/filters/FiltersViewModel.kt
@@ -3,4 +3,13 @@ package com.stathis.foodie.ui.dashboard.filters
 import androidx.lifecycle.ViewModel
 
 class FiltersViewModel : ViewModel() {
+
+    fun checkIfValid(
+        calorieMin: String,
+        calorieMax: String,
+        mealType: String,
+        dietType: String
+    ): Any {
+        return calorieMin.isNotEmpty() && calorieMax.isNotEmpty() && mealType.isNotEmpty() && dietType.isNotEmpty()
+    }
 }
