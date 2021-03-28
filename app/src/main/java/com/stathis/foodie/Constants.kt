73c45e91bb89fcbf5d42659fd0dcb71302ae0927
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2021-03-28 17:29:23 +0300

    Project resources cleanup

--- app/src/main/java/com/stathis/foodie/Constants.kt
+++ app/src/main/java/com/stathis/foodie/Constants.kt
@@ -1,8 +1,6 @@
 package com.stathis.foodie
 
-const val OLD = "&app_id=57e6d292&app_key=000b571f7a78bfb4fb9820a7cc3b283e"
+const val FULL_APP_ID_AND_KEY = "&app_id=57e6d292&app_key=000b571f7a78bfb4fb9820a7cc3b283e"
 
 const val APP_ID = "57e6d292"
 const val APP_KEY = "000b571f7a78bfb4fb9820a7cc3b283e"
-
-const val TAG = "OKAY"
--- app/src/main/java/com/stathis/foodie/models/SuggestionItem.kt
+++ app/src/main/java/com/stathis/foodie/models/SuggestionItem.kt
@@ -1,7 +1,5 @@
 package com.stathis.foodie.models
 
-import com.stathis.foodie.models.LocalModel
-
 class SuggestionItem(
     val categoryName: String,
     val categoryImg: Int
