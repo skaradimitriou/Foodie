b50441bd0fe3b57710ca0f8b410c197392908224
Stathis Karaimitriou <stathis.karadimitriou@gmail.com>
2021-04-19 23:03:14 +0300

    Categories Screen | Added every options

--- app/src/main/java/com/stathis/foodie/ui/categories/CategoriesResultsViewModel.kt
+++ app/src/main/java/com/stathis/foodie/ui/categories/CategoriesResultsViewModel.kt
@@ -38,11 +38,11 @@ class CategoriesResultsViewModel : ViewModel(), ItemClickListener {
 
     fun getResults(cuisineType: String) {
         isLoading.value = true
-        val cleanCuisineType = cuisineType.replace("\\s".toRegex(), "")
-        when (cleanCuisineType) {
-            "breakfast", "lunch","dinner","snack","teatime" -> repo.getMealTypeResults(cleanCuisineType)
-            "soup", "salad", "dessert","cereals","pancake","starter","omelet" -> repo.getDishTypeResults(cleanCuisineType)
-            "indian", "chinese", "italian","mexican","japanese" -> repo.getCuisineTypeResults(cleanCuisineType)
+        val newCuisineType = cuisineType.replace("\\s".toRegex(), "")
+        when (newCuisineType) {
+            "breakfast", "lunch","dinner","snack","teatime" -> repo.getMealTypeResults(newCuisineType)
+            "soup", "salad", "dessert","cereals","pancake","starter","omelet" -> repo.getDishTypeResults(newCuisineType)
+            "indian", "chinese", "italian","mexican","japanese" -> repo.getCuisineTypeResults(newCuisineType)
             else -> Unit
         }
     }
